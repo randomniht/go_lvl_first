efc910372af87c03bcae0fa9b262cb66ef0a000e
randomniht <11s13tayr@gmail.com>
2026-03-09 23:40:15 +0500

    var

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func main_new() {
+	var (
+		name string = "Tom"
+		age  int    = 27
+	)
+
+	test := "new"
+	fmt.Println(test)
+	fmt.Println(name)
+	fmt.Println(age)
+}
+
 func main() {
 	fmt.Println("commit test")
+	main_new()
 }
